7ddc5b5f2cd3bd289c110c193f3c3283c1f6a3f9
Anselem <anselemodims@gmail.com>
2022-02-08 22:59:55 +0100

    included the add function that is invoked

---
+++ Module/add.js
@@ -0,0 +1,5 @@
+function add(num1, num2){
+   console.log(`The sum is ${num1+num2}`)
+}
+
+add(2,3)
--- Module/main.js
+++ Module/main.js
@@ -1,5 +1,6 @@
 const names = require('./name.js');
 const sayHi = require('./utils.js');
+require('./add')
 
 sayHi(names.man);
 sayHi(names.woman);
